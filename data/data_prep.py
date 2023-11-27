f77e718355f216f3217b1fa15a94fbb8e2e6b5bd
Joel Akiyoshi <akiyoshj@uci.edu>
2023-11-27 13:56:46 -0800

    built DataHandler class

--- data/data_prep.py
+++ data/data_prep.py
@@ -8,46 +8,49 @@ from keras.utils import load_img, img_to_array
 from keras.applications.vgg16 import preprocess_input
 
 
-def label_images(parent_directory: str) -> tuple:
-    """Return an array of images, and an array of labels in a two-tuple.
-    Input is a directory path containing the classification folders for
-    either training/testing. Walks through the directory, prepares the images
-    and stores them."""
+class DataHandler:
+    def __init__(self, parent_directory):
+        self.parent_directory = parent_directory
+        self.label_encoder = preprocessing.LabelEncoder()
 
-    # labeling training data
+    def get_images_and_labels(self) -> tuple:
+        """Return an array of images, and an array of labels in a two-tuple.
+        Input is a directory path containing the classification folders for
+        either training/testing. Walks through the directory, prepares the images
+        and stores them."""
 
-    images = []
-    labels = []
+        # labeling training data
 
-    # 'maize_split/train/*'
-    # 'maize_split/val/*'
-    for directory_path in glob.glob(parent_directory + '/*'):
+        images = []
+        labels = []
 
-        # get the disease name
-        classification = directory_path.split('\\')[-1]
+        # 'maize_split/train/*'
+        # 'maize_split/val/*'
+        for directory_path in glob.glob(self.parent_directory + '/*'):
 
-        for image_path in glob.glob(directory_path + '/*'):
-            img = load_img(image_path, target_size=(256, 256))
-            arrayed_img = img_to_array(img)
-            proc_img = preprocess_input(arrayed_img)
+            # get the disease name
+            classification = directory_path.split('\\')[-1]
 
-            labels.append(classification)
-            images.append(proc_img)
+            for image_path in glob.glob(directory_path + '/*'):
+                img = load_img(image_path, target_size=(256, 256))
+                arrayed_img = img_to_array(img)
+                proc_img = preprocess_input(arrayed_img)
 
-    # convert to numpy array
-    images = np.array(images)
-    labels = np.array(labels)
+                labels.append(classification)
+                images.append(proc_img)
 
-    return images, labels
+        # convert to numpy array
+        images = np.array(images)
+        labels = np.array(labels)
 
+        return images, labels
 
-def encode_labels(labels):
-    """convert the labels to encoded values instead of str"""
-    label_encoder = preprocessing.LabelEncoder()
-    label_encoder.fit(labels)
-    labels_encoded = label_encoder.transform(labels)
-    return labels_encoded
+    def get_encoded_labels(self, original_labels):
+        """convert the labels to encoded values (0, 1, 2, 3)"""
+        encoded_labels = self.label_encoder.fit_transform(original_labels)
+        return encoded_labels
 
-
-def decode_labels(labels: int):
-    pass
+    def decode_labels(self, encoded_labels):
+        """convert the labels to original values (Blight, Rust, Spot, Normal)"""
+        original_labels = self.label_encoder.inverse_transform(encoded_labels)
+        return original_labels
